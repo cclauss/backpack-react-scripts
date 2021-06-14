b2e6c9f9b2cbb1037c9bb7479201d22dd39000b7
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2021-06-14 15:19:38 +0100

    [NO JIRA]: Adding new paths for scoped Backpack packages (#97)

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -114,6 +114,7 @@ const hasJsxRuntime = (() => {
 // Backpack / saddlebag node module regexes
 const backpackModulesRegex = /node_modules[\\/]bpk-/;
 const saddlebagModulesRegex = /node_modules[\\/]saddlebag-/;
+const scopedBackpackModulesRegex = /node_modules[\\/]@skyscanner[\\/]bpk-/;
 
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
@@ -462,12 +463,13 @@ module.exports = function (webpackEnv) {
                 paths.appSrc,
                 backpackModulesRegex,
                 saddlebagModulesRegex,
+                scopedBackpackModulesRegex,
                 ...customModuleRegexes,
               ],
               loader: require.resolve('babel-loader'),
               options: {
                 customize: require.resolve(
-                  'babel-preset-react-app/webpack-overrides',
+                  'babel-preset-react-app/webpack-overrides'
                 ),
                 presets: [
                   [
@@ -494,7 +496,7 @@ module.exports = function (webpackEnv) {
                     'babel-preset-react-app',
                     'react-dev-utils',
                     'react-scripts',
-                  ],
+                  ]
                 ),
                 // @remove-on-eject-end
                 plugins: [
@@ -531,6 +533,7 @@ module.exports = function (webpackEnv) {
                 paths.appSrc,
                 backpackModulesRegex,
                 saddlebagModulesRegex,
+                scopedBackpackModulesRegex,
                 ...customModuleRegexes,
               ],
               use: [
@@ -548,7 +551,7 @@ module.exports = function (webpackEnv) {
                   loader: require.resolve('babel-loader'),
                   options: {
                     customize: require.resolve(
-                      'babel-preset-react-app/webpack-overrides',
+                      'babel-preset-react-app/webpack-overrides'
                     ),
                     presets: [
                       [
@@ -575,7 +578,7 @@ module.exports = function (webpackEnv) {
                         'babel-preset-react-app',
                         'react-dev-utils',
                         'react-scripts',
-                      ],
+                      ]
                     ),
                     // @remove-on-eject-end
                     plugins: [
@@ -631,7 +634,7 @@ module.exports = function (webpackEnv) {
                         'babel-preset-react-app',
                         'react-dev-utils',
                         'react-scripts',
-                      ],
+                      ]
                     ),
                     // @remove-on-eject-end
                     // Babel sourcemaps are needed for debugging into node_modules
@@ -653,7 +656,7 @@ module.exports = function (webpackEnv) {
             {
               test: {
                 and: [cssRegex, () => !cssModulesEnabled],
-                exclude: [backpackModulesRegex],
+                exclude: [backpackModulesRegex, scopedBackpackModulesRegex],
               },
               exclude: cssModuleRegex,
               use: getStyleLoaders({
@@ -680,7 +683,11 @@ module.exports = function (webpackEnv) {
                   and: [cssRegex, () => cssModulesEnabled],
                 },
                 {
-                  and: [cssRegex, backpackModulesRegex],
+                  and: [
+                    cssRegex,
+                    backpackModulesRegex,
+                    scopedBackpackModulesRegex,
+                  ],
                 },
               ],
               use: getStyleLoaders({
@@ -700,7 +707,7 @@ module.exports = function (webpackEnv) {
             {
               test: {
                 and: [sassRegex, () => !cssModulesEnabled],
-                exclude: [backpackModulesRegex],
+                exclude: [backpackModulesRegex, scopedBackpackModulesRegex],
               },
               exclude: sassModuleRegex,
               use: getStyleLoaders(
@@ -733,7 +740,11 @@ module.exports = function (webpackEnv) {
                   and: [sassRegex, () => cssModulesEnabled],
                 },
                 {
-                  and: [sassRegex, backpackModulesRegex],
+                  and: [
+                    sassRegex,
+                    backpackModulesRegex,
+                    scopedBackpackModulesRegex,
+                  ],
                 },
               ],
               use: getStyleLoaders(
