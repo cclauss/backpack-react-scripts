9520cf89d3645f65ba5aeff0470a900117aaa576
sophia-chen <sophia.chen@skyscanner.net>
2022-01-11 11:58:58 +0800

    revert format

--- packages/react-scripts/config/webpack.config.ssr.js
+++ packages/react-scripts/config/webpack.config.ssr.js
@@ -211,9 +211,7 @@ module.exports = function (webpackEnv) {
           {
             loader: require.resolve('resolve-url-loader'),
             options: {
-              sourceMap: isEnvProduction
-                ? shouldUseSourceMap
-                : isEnvDevelopment,
+              sourceMap: isEnvProduction ? shouldUseSourceMap : isEnvDevelopment,
               root: paths.appSrc,
             },
           },
