b634dfcb8a196b729243c4487e4d2a3f10dd1dc6
Salley Liu <salley.liu@skyscanner.net>
2022-02-22 17:11:05 +0800

    update runtimeChunk

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -380,7 +380,7 @@ module.exports = function (webpackEnv) {
               : {},
           }
         : {},
-      ...require('../backpack-addons/runtimeChunk')(), // #backpack-addons runtimeChunk
+      ...require('../backpack-addons/runtimeChunk')('build'), // #backpack-addons runtimeChunk
     },
     externals: isEnvProduction ? bpkReactScriptsConfig.externals || {} : {},
     resolve: {
