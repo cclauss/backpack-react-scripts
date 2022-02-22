bf6606fb6f4fd1975d93d9b1b1d97750367acbf1
Salley Liu <salley.liu@skyscanner.net>
2022-02-22 15:50:13 +0800

    update ssr sassFunctions

--- packages/react-scripts/config/webpack.config.ssr.js
+++ packages/react-scripts/config/webpack.config.ssr.js
@@ -58,7 +58,6 @@ const getCSSModuleLocalIdent = require('../utils/getCSSModuleLocalIdentWithProje
   appPackageJson.name
 );
 
-const sassFunctions = require('../utils/sassFunction');
 // const camelCase = require('lodash/camelCase');
 const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
 const customModuleRegexes = bpkReactScriptsConfig.babelIncludePrefixes
@@ -744,11 +743,7 @@ module.exports = function (webpackEnv) {
                     : isEnvDevelopment,
                 },
                 'sass-loader',
-                {
-                  sassOptions: {
-                    functions: sassFunctions,
-                  },
-                }
+                require('../backpack-addons/sassFunctions') // #backpack-addons sassFunctions
               ),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
