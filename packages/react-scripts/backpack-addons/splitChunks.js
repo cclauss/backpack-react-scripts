93053d314543eecbc7a4a229378f4523bbb174b7
Salley Liu <salley.liu@skyscanner.net>
2022-02-28 11:07:24 +0800

    add splitChunk annotation

--- packages/react-scripts/backpack-addons/splitChunks.js
+++ packages/react-scripts/backpack-addons/splitChunks.js
@@ -5,6 +5,9 @@ const appPackageJson = require(paths.appPackageJson);
 const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
 
 module.exports = (isEnvDevelopment) => {
+  // Automatically split vendor and commons
+  // https://twitter.com/wSokra/status/969633336732905474
+  // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
   return {
     splitChunks: bpkReactScriptsConfig.enableAutomaticChunking
     ? {
