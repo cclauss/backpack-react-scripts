d708e1a5ff57f38380bc7d8715c569d66bc7614c
salleyliu <86700834+salleyliu@users.noreply.github.com>
2022-03-01 14:26:59 +0800

    [LEOP-317]Extract sassFunctions (#125)
    * sassFunctions addon
    
    * remove utils/sassFunctions.js
    
    * update ssr sassFunctions
    
    * update ssr sassFunctions

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -39,7 +39,6 @@ const createEnvironmentHash = require('./webpack/persistentCache/createEnvironme
 
 const appPackageJson = require(paths.appPackageJson);
 
-const sassFunctions = require('../utils/sassFunction');
 const camelCase = require('lodash/camelCase');
 const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
 
@@ -676,11 +675,7 @@ module.exports = function (webpackEnv) {
                   },
                 },
                 'sass-loader',
-                {
-                  sassOptions: {
-                    functions: sassFunctions,
-                  },
-                }
+                require('../backpack-addons/sassFunctions')  // #backpack-addons sassFunctions
               ),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
@@ -706,11 +701,7 @@ module.exports = function (webpackEnv) {
                   },
                 },
                 'sass-loader',
-                {
-                  sassOptions: {
-                    functions: sassFunctions,
-                  },
-                }
+                require('../backpack-addons/sassFunctions')  // #backpack-addons sassFunctions
               ),
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
