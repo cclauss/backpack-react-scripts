2a51c65903d62ad7f48f77be417fd9368de4ba4a
salleyliu <86700834+salleyliu@users.noreply.github.com>
2022-03-01 11:59:46 +0800

    [LEOP-317]Extract splitChunks (#127)
    * extract splitChunks
    
    * update require path
    
    * add splitChunk annotation

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -330,6 +330,18 @@ module.exports = function (webpackEnv) {
         // This is only used in production mode
         new CssMinimizerPlugin(),
       ],
+      ...require('../backpack-addons/splitChunks')(isEnvDevelopment),  // #backpack-addons splitChunks
+      // Keep the runtime chunk separated to enable long term caching
+      // https://twitter.com/wSokra/status/969679223278505985
+      // https://github.com/facebook/create-react-app/issues/5358
+      // runtimeChunk: {
+      //   name: entrypoint => `runtime-${entrypoint.name}`,
+      // },
+      runtimeChunk: bpkReactScriptsConfig.enableAutomaticChunking
+        ? {
+            name: entrypoint => `runtime-${entrypoint.name}`,
+          }
+        : false,
     },
     externals: isEnvProduction ? bpkReactScriptsConfig.externals || {} : {},
     resolve: {
