15f27c36c2ba7e68d2350c53f234444114b84973
Tao Fang <85286316+taofang2021@users.noreply.github.com>
2022-04-07 17:29:34 +0800

    [LEOP-294] Update node to 16.x in workflow (#135)
    * [LEOP-294] Update node to 16.x in workflow
    
    Co-authored-by: tao.fang <tao.fang@skyscanner.net>

--- tasks/e2e-installs.sh
+++ tasks/e2e-installs.sh
@@ -117,12 +117,12 @@ checkDependencies
 # ******************************************************************************
 
 cd "$temp_app_path"
-npx create-react-app test-app-version-number --scripts-version=backpack-react-scripts@8.0.0-beta.41a3a687f --template @skyscanner/backpack
+npx create-react-app test-app-version-number --scripts-version=@skyscanner/backpack-react-scripts@9.7.0-beta.5 --template @skyscanner/backpack
 cd test-app-version-number
 
 # Check corresponding scripts version is installed.
-exists node_modules/backpack-react-scripts
-grep '"version": "8.0.0-beta.41a3a687f"' node_modules/backpack-react-scripts/package.json
+exists node_modules/@skyscanner/backpack-react-scripts
+grep '"version": "9.7.0-beta.5"' node_modules/@skyscanner/backpack-react-scripts/package.json
 checkDependencies
 
 # ******************************************************************************
@@ -130,13 +130,13 @@ checkDependencies
 # ******************************************************************************
 
 cd "$temp_app_path"
-npx create react-app test-use-yarn-create --scripts-version=@skyscanner/backpack-react-scripts@9.1.0 --template @skyscanner/backpack
+npx create react-app test-use-yarn-create --scripts-version=@skyscanner/backpack-react-scripts@10.0.0 --template @skyscanner/backpack
 cd test-use-yarn-create
 
 # Check corresponding scripts version is installed.
 exists node_modules/@skyscanner/backpack-react-scripts
 exists yarn.lock
-grep '"version": "9.1.0"' node_modules/@skyscanner/backpack-react-scripts/package.json
+grep '"version": "10.0.0"' node_modules/@skyscanner/backpack-react-scripts/package.json
 checkDependencies
 
 # ******************************************************************************
