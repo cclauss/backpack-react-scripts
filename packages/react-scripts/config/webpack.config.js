a8561acf486af7ce23e723773b8c7c430cf503ca
Salley Liu <salley.liu@skyscanner.net>
2022-04-20 15:15:00 +0800

    apply amdExcludes to CRA5

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -371,6 +371,7 @@ module.exports = function (webpackEnv) {
                 },
               },
             },
+            require('../backpack-addons/amdExcludes'),  // #backpack-addons amdExcludes
             // "url" loader works like "file" loader except that it embeds assets
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
