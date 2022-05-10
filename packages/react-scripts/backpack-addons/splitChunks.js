ab82a118364efd5500a7f9ab27a5559d58e1362c
salleyliu <86700834+salleyliu@users.noreply.github.com>
2022-05-10 16:35:30 +0800

    Update splitChunks.js

--- packages/react-scripts/backpack-addons/splitChunks.js
+++ packages/react-scripts/backpack-addons/splitChunks.js
@@ -4,39 +4,31 @@ const paths = require('../config/paths');
 const appPackageJson = require(paths.appPackageJson);
 const bpkReactScriptsConfig = appPackageJson['backpack-react-scripts'] || {};
 
+const chunksAndGroups = {
+  chunks: 'all',
+  cacheGroups: bpkReactScriptsConfig.vendorsChunkRegex
+    ? {
+        defaultVendors: {
+          test: new RegExp(bpkReactScriptsConfig.vendorsChunkRegex),
+        },
+      }
+    : {},
+};
+
 module.exports = (isEnvDevelopment) => {
   // Automatically split vendor and commons
   // https://twitter.com/wSokra/status/969633336732905474
   // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-  if(isEnvDevelopment){
-    return {
-      splitChunks: bpkReactScriptsConfig.enableAutomaticChunking
-        ? {
-            chunks: 'all',
-            cacheGroups: bpkReactScriptsConfig.vendorsChunkRegex
-              ? {
-                  defaultVendors: {
-                    test: new RegExp(bpkReactScriptsConfig.vendorsChunkRegex),
-                  },
-                }
-              : {},
-          }
-        : {},
-    };
-  };
   return {
     splitChunks: bpkReactScriptsConfig.enableAutomaticChunking
-      ? {
-          chunks: 'all',
-          name: false,
-          cacheGroups: bpkReactScriptsConfig.vendorsChunkRegex
-            ? {
-                defaultVendors: {
-                  test: new RegExp(bpkReactScriptsConfig.vendorsChunkRegex),
-                },
-              }
-            : {},
-        }
+      ? isEnvDevelopment 
+        ? {
+            ...chunksAndGroups,
+          }
+        : {
+            ...chunksAndGroups,
+            name: false,
+          }
       : {},
   };
 };
